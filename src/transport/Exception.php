47ebe06ad8a4f949fef920254f76c80ba7fb2314
Pavel Terentyev <terentev.p@sibvaleo.com>
2017-07-17 18:11:19 +0700

    SMTP stub extended

---
+++ src/transport/Exception.php
@@ -0,0 +1,11 @@
+<?php namespace Micro\Mail\Transport;
+/**
+ * @link https://github.com/linpax/microphp-mail
+ * @copyright Copyright &copy; 2017 Linpax
+ * @license https://github.com/linpax/microphp-mail/blob/master/LICENSE
+ */
+
+class Exception extends \Micro\Mail\Exception
+{
+
+}
--- src/transport/SmtpTransport.php
+++ src/transport/SmtpTransport.php
@@ -13,6 +13,52 @@ use Micro\Mail\TransportInterface;
 
 class SmtpTransport implements TransportInterface
 {
+    /**
+     * SMTP params for transport
+     * host
+     * port
+     * login
+     * password
+     * @var array
+     *
+     */
+    protected $params = [];
+
+    /**
+     * TODO: Запилить драйвер SMTP
+     * @var
+     */
+    protected $connection;
+    public function __construct($params)
+    {
+        foreach ($params as $name=>$value){
+            $this->setParam($name,$value);
+        }
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     * @return $this
+     */
+    public function setParam($name, $value)
+    {
+        $this->params[$name] = $value;
+        return $this;
+    }
+
+    /**
+     * @param $name
+     * @return mixed
+     */
+    public function getParam($name)
+    {
+        if (isset($this->params[$name])){
+            return $this->params[$name];
+        }
+        return false;
+    }
+
     public function send(Mail $message)
     {
         // TODO: Implement send() method.
